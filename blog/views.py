6ac4940684eea020b46aedf0fc5a271e9b6a8eff
Jean Liu <jean.liu415@gmail.com>
2017-03-26 15:35:15 -0700

    added in query for Post

--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+Post.objects.get(pk=pk)
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
